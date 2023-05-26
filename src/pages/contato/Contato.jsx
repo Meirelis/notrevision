edce727bb369013e95a60d6afc0e471b63d8c6a3
behsant <brenasantfbj@gmail.com>
2023-05-26 19:37:16 -0300

    novo

--- src/pages/contato/Contato.jsx
+++ src/pages/contato/Contato.jsx
@@ -6,18 +6,19 @@ function Contato(){
   
     return(
         <>
-       <div>
-        <h1>Formulário</h1>
-        
-        </div>
+       <div className='tudo'>
+        <h1>Entre em contato conosco</h1>
+       
+        <div className='sla'>
         <form>
-            <TextField id="nome" label="Nome" variant="standard" /> <br></br>
-            <TextField id="email" label="E-mail" variant="standard" /> <br></br>
+            <TextField id="nome" label="Nome" variant="standard" /> <br></br><br></br>
+            <TextField id="email" label="E-mail" variant="standard" /> <br></br><br></br>
             <TextField id="Telefone" label="Telefone" variant="standard" /><br></br>
 
         </form>
         
-        <br></br> <Button variant="contained" size="small">Enviar</Button>
+        <br/><br/> <Button variant="contained" size="small">Enviar</Button></div> 
+        </div>
         </>
        
       )  
