6b45dc032c38bc23696e7f00402772962e315845
Alaxdav1d <alaxdavid13@gmail.com>
2023-05-25 22:13:17 -0300

    adicionando components a tela de produtos

--- src/components/prods/Prods.jsx
+++ src/components/prods/Prods.jsx
@@ -1,7 +1,16 @@
-import './Prods.css'
-function Prods(){
+import './Prods.css';
+
+import oculos1 from "../../assets/imgOculos1.png";
+
+function Prods(props){
     return (
-        <h1>Ola mundo do prods</h1>
+      <div className='prods'>
+      
+        <img src={props.img ? props.img : oculos1} alt="" />
+        <p className='p1'>R$150,00</p>
+        <p className='p2'>10X sem juros</p>
+     
+      </div>
     )
 }
 
--- src/pages/Produto/Produto.jsx
+++ src/pages/Produto/Produto.jsx
@@ -1,26 +1,35 @@
 import Prods from "../../components/prods/prods";
 import "./Produto.css";
 
+import oculos2 from "../../assets/imgOculos2.png";
+import oculos3 from "../../assets/imgOculos3.png";
+import oculos4 from "../../assets/imgOculos4.png";
 
 function Produto(){
     return (
-        <>
+        <div className="tudo">
             <div className="cima">
-            <Prods/>
-
-            <Prods/>
-
-
-            <Prods/>
-
-
-            <Prods/>
+                <Prods img={oculos3}/>
+                <Prods img={oculos2}/>
+                <Prods img={oculos4}/>
+                <Prods/>
+                <Prods/>
+                <Prods/>
+                <Prods/>
+                <Prods/>
 
             </div>
             <div className="baixo">
-
+                <Prods img={oculos2}/>
+                <Prods />
+                <Prods img={oculos4}/>
+                <Prods/>
+                <Prods/>
+                <Prods/>
+                <Prods/>
+                <Prods/>
             </div>
-        </>
+        </div>
     )
 }
 export default Produto;
