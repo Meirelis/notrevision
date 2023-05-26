1a80eeaca8bdd8f4d3c32284f8653a6724240e3c
Alaxdav1d <alaxdavid13@gmail.com>
2023-05-26 19:53:07 -0300

    teste

--- src/pages/Produto/Produto.jsx
+++ src/pages/Produto/Produto.jsx
@@ -1,9 +1,6 @@
 import Prods from "../../components/prods/prods";
 import "./Produto.css";
-
-import oculos2 from "../../assets/imgOculos2.png";
 import oculos3 from "../../assets/imgOculos3.png";
-import oculos4 from "../../assets/imgOculos4.png";
 import { Box, Grid } from "@mui/material";
 
 function Produto() {
