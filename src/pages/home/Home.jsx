ca658446302d9cf6d64aaaebd2d1ffaf2af7e234
Meirelis <zmeireles717@gmail.com>
2023-05-30 20:06:49 -0300

    finish

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,8 @@ import "./home.css";
 import emoji from '../../assets/emogi-oculos.png';
 import Footer from "../../components/footer/Footer";
 import Carousel from "react-material-ui-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import abacaxi from '../../assets/emogi-oculos.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 
@@ -10,43 +11,47 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 function Home() {
 
     return (
-        <>
-        <div className="container-banner">
-            <div className="bg-imag">
+        <div className="tudoHome">
+            <div className="container-banner">
+                <div className="bg-imag">
+                </div>
+
+                <div className="texto-banner">
+                    <p className="big">O estilo é ser</p>
+                    <p className="big-voce"><strong>VOCÊ</strong></p>
+                    <p className="texto-legenda"><strong>Liberte-se</strong> das limitações e aproveite a vida com clareza e conforto</p>
+                </div>
             </div>
 
-             <div className="texto-banner">
-                <p className="big">O estilo é ser</p>
-                <p className="big-voce"><strong>VOCÊ</strong></p>
-                <p className="texto-legenda"><strong>Liberte-se</strong> das limitações e aproveite a vida com clareza e conforto</p>
-             </div>         
-         </div>
 
-         
 
-         <div className="divisor"></div>
-
-        <div className="tudo1">
-        <div className="txtCar">
-                
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate eveniet modi nihil illo? Vero minima impedit ut ex molestiae facere hic voluptatum voluptas odit deleniti nemo, mollitia blanditiis repellendus obcaecati?Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, optio consequatur? Expedita fuga adipisci unde, voluptatem sed voluptatum maxime qui deleniti ratione optio pariatur reiciendis consequuntur maiores, nostrum quisquam corporis. 
-            </p>
-         
-            <div className="imgs-mid">
-               <Carousel className="carrosel">
-                   <div className="abacaxi"></div>
-                   <div className="garotinho"></div>
-                   <div className="hamilton"></div>
-               </Carousel> 
-            </div>  
-        </div>
-        </div>
-       
-                  
-  
-        </>
+            <div className="divisor"></div>
 
         
+            <div className="txtCar">
+
+                <div className="imgs-mid">
+                    <Carousel className="carrosel">
+                        <div className="garotinho"></div>
+                        <div className="hamilton"></div>
+                    </Carousel>
+                </div>
+                <div className="texto-car">
+                    <p>Bem-vindo ao site da nossa ótica! Aqui você irá descobrir os serviços e produtos que oferecemos.
+                        Nossa equipe de especialistas em ótica está comprometida em fornecer a você uma experiência visual de alta qualidade, garantindo que suas necessidades sejam atendidas da melhor forma possível.
+                        Nossos serviços abrangem desde exames de visão abrangentes até a seleção e adaptação de lentes de contato e óculos. Com equipamentos modernos e uma abordagem personalizada, nossa equipe está pronta para ajudá-lo a alcançar a melhor experiência visual e o conforto visual ideal.
+                    </p>
+                </div>
+
+
+            </div>
+
+
+
+
+        </div>
+
+
     )
 }
 
