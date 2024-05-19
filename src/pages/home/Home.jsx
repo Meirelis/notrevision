9d6847811c2506d09cef5bb500bb8a04d703089a
AlaxDav1d <alaxdavid13@gmai.com>
2024-05-18 21:33:32 -0300

    deixei a primeira pagina responsiva

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,31 @@
 import "./home.css";
 import Carousel from "react-material-ui-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import moca from "../../assets/mocinha-oculos.png"
-
-function Home() {
-
+import moca from "../../assets/mocinha-oculos.png";
+import abacaxi from "../../assets/abacaxi.png";
+function Home(){
     return (
-
         <div className="tudoHome">
-           
             <div className="bg-imag">
-                    <img src={moca}/>
+                    <img src={moca} id="moca" />
+                    <img src={abacaxi} alt="abacaxi de oculos de sol" id="abacaxi" />
                     <div className="texto-banner">
                         <h1>O estilo é ser</h1>
                         <h2>VOCÊ</h2>
                         <p><strong>Liberte-se</strong> das limitações e aproveite a vida com clareza e conforto</p>
-                    </div>    
-
+                    </div> 
             </div>
-            <div className="divisor"></div>
-
             <div className="txtCar">
                 <div className="imgs-mid">
-                    <Carousel className="carrosel">
+                    <Carousel indicatorIconButtonProps={{style: {display:'none'}}}>
                         <div className="garotinho"></div>
                         <div className="hamilton"></div>
                     </Carousel>
                 </div>
                 <div className="texto-car">
                     <p> Bem-vindo ao site da nossa ótica! Aqui você irá descobrir os serviços e produtos que oferecemos.</p>
-                    <p>Nossa equipe de especialistas em ótica está comprometida em fornecer a você uma experiência visual de alta qualidade, garantindo que suas necessidades sejam atendidas da melhor forma possível.</p>
-                    <p> Nossos serviços abrangem desde exames de visão abrangentes até a seleção e adaptação de lentes de contato e óculos. Com equipamentos modernos e uma abordagem personalizada, nossa equipe está pronta para ajudá-lo a alcançar a melhor experiência visual e o conforto visual ideal.</p>
-                       
-                    
+                    <p> Nossa equipe de especialistas em ótica está comprometida em fornecer a você uma experiência visual de alta qualidade, garantindo que suas necessidades sejam atendidas da melhor forma possível.</p>
+                    <p> Nossos serviços abrangem desde exames de visão abrangentes até a seleção e adaptação de lentes de contato e óculos. Com equipamentos modernos e uma abordagem personalizada, nossa equipe está pronta para ajudá-lo a alcançar a melhor experiência visual e o conforto visual ideal.</p>        
                 </div>
             </div>
 
